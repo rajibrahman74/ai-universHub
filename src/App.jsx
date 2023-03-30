412ed53e1678110057709a4dfd3e5ec4f844897e
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-31 01:20:37 +0600

    [FIX]-delete-app.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Button from "./components/Button/Button";
 import Card from "./components/Card/Card";
 import Header from "./components/Header/Header";
-import Modal from "./components/Modal/Modal";
 
 const App = () => {
   return (
