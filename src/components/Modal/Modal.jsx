712bffb5acc9759d3cdb794e2e6c547466a05ce8
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-31 01:17:46 +0600

    [ADD]-complete-modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 
 const Modal = (props) => {
-  console.log(props.singleData);
-  const {image_link, features, description, accuracy, integrations} = props.singleData;
+  const { image_link, features, integrations, description, accuracy } =
+    props.singleData;
   return (
     <React.Fragment>
       <input type="checkbox" id="my-modal-5" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box w-11/12 max-w-5xl">
           <div className="card lg:card-side bg-base-100">
-            <figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                {description ? description : "Description Not found"}
+              </h2>
+              <div className="flex justify-between py-3">
+                <div>
+                  <h3 className="card-title">Features</h3>
+                  {Object.values(features || {}).map((value) => (
+                    <li>{value.feature_name}</li>
+                  ))}
+                </div>
+
+                <div>
+                  <h3 className="card-title">Integrations</h3>
+                </div>
+              </div>
+            </div>
+            <figure className="w-full">
               <img
+                className="w-full h-96"
                 src={image_link && image_link[0]}
                 alt="Album"
               />
             </figure>
-            <div className="card-body">
-              <h2 className="card-title">New album is released!</h2>
-              <p>Click the button to listen on Spotiwhy app.</p>
-              <div className="card-actions justify-end">
-                <button className="btn btn-primary">Listen</button>
-              </div>
-            </div>
           </div>
           <div className="modal-action">
             <label htmlFor="my-modal-5" className="btn">
