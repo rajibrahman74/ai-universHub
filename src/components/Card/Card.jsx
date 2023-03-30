00da8e1e1c32222ef32c80ecf5aa32b6263dea13
rajibrahman74e <rajiburrahman74@gmail.com>
2023-03-30 22:28:33 +0600

    [ADD]-load-all-data-in-card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,7 +12,7 @@ const Card = () => {
 
   return (
     <React.Fragment>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-6">
         {data.map((singleData) => (
           <Data key={singleData.id} singleData={singleData}></Data>
         ))}
